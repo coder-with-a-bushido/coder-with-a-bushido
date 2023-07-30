ee1ff276763af203a88c8ca416576969a2c53ac9
Karthikeyan S <karthikeyanssvk@gmail.com>
2023-07-30 18:30:41 +0530

    fix icon

--- src/config.ts
+++ src/config.ts
@@ -11,13 +11,6 @@ export const SITE: Site = {
 
 export const LOCALE = ["en-EN"]; // set to [] to use the environment default
 
-export const LOGO_IMAGE = {
-  enable: false,
-  svg: true,
-  width: 216,
-  height: 46,
-};
-
 export const SOCIALS: SocialObjects = [
   {
     name: "Mail",

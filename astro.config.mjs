71752d0a0b546e4ba53016163fe6567ff9161fe9
Karthikeyan S <karthikeyanssvk@gmail.com>
2023-09-17 11:37:08 +0530

    add vercel analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,17 +4,24 @@ import react from "@astrojs/react";
 import remarkToc from "remark-toc";
 import remarkCollapse from "remark-collapse";
 import sitemap from "@astrojs/sitemap";
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://coder-with-a-bushido.in/",
   adapter: vercel({
-    analytics: true,
+    webAnalytics: {
+      enabled: true,
+    },
+    speedInsights: {
+      enabled: true,
+    },
   }),
   redirects: {
     "/meet": "https://bushi.app.100ms.live/meeting/bet-herx-yse",
     "/book": "https://cal.com/coder-with-a-bushido",
-    "/resume": "https://drive.google.com/file/d/1mRPCr5pvXpC6i0Krx2Txa_HsHwaTMjYB/view",
+    "/resume":
+      "https://drive.google.com/file/d/1mRPCr5pvXpC6i0Krx2Txa_HsHwaTMjYB/view",
   },
   experimental: {
     redirects: true,
